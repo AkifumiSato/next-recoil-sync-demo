97f651dcf9cff9318d765beb0e4f0797a7266fd8
akfm.sato <akifumi_sato@r.recruit.co.jp>
2022-06-24 15:29:25 +0900

    add recoil-sync

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
+import { RecoilURLSyncJSON } from 'recoil-sync'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <RecoilRoot>
-      <Component {...pageProps} />
+      <RecoilURLSyncJSON
+        location={{ part: 'queryParams' }}
+        browserInterface={{
+          getURL: () => `http://localhost:3000${router.asPath}`,
+        }}
+      >
+        <Component {...pageProps} />
+      </RecoilURLSyncJSON>
     </RecoilRoot>
   )
 }
--- src/components/recoil.ts
+++ src/components/recoil.ts
@@ -1,6 +1,9 @@
 import { atom } from 'recoil'
+import { syncEffect } from 'recoil-sync'
+import { number } from '@recoiljs/refine'
 
 export const countState = atom({
   key: 'count',
   default: 0,
+  effects: [syncEffect({ refine: number() })],
 })
